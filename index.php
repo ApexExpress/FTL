c6b3c1ff979292b46c58b46f5a9f231e9bf43cc0
CJ <132447345+ApexExpress@users.noreply.github.com>
2023-07-03 22:20:51 -0600

    Create index.php
    php index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+<head>
+<title> Dash Application </title>
+<meta name="title" content="example" />
+<link rel="stylesheet" href="www.example.com/css/css.css" type="text/css" />
+</head>
+
+?>
