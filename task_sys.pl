98c87357095713bb4093e1137cfa4bc2ab31df97
CJ <132447345+ApexExpress@users.noreply.github.com>
2023-07-04 18:59:35 -0600

    Create task_sys.pl
    task system inplementation

---
+++ task_sys.pl
@@ -0,0 +1,26 @@
+import time
+
+def task1():
+    print("Performing Task 1...")
+    # Add your code for Task 1 here
+    time.sleep(2)  # Simulating some processing time
+
+def task2():
+    print("Performing Task 2...")
+    # Add your code for Task 2 here
+    time.sleep(3)  # Simulating some processing time
+
+def task3():
+    print("Performing Task 3...")
+    # Add your code for Task 3 here
+    time.sleep(1)  # Simulating some processing time
+
+def main():
+    print("Starting Automation Intelligence Script...")
+    task1()
+    task2()
+    task3()
+    print("Automation Intelligence Script completed.")
+
+if __name__ == '__main__':
+    main()
