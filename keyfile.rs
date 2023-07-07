cbf5663b0511557268d4724eea9b14e63e89cf24
CJ <132447345+ApexExpress@users.noreply.github.com>
2023-07-07 11:24:21 -0700

    Create keyfile.rs
    add chatgpt file

---
+++ keyfile.rs
@@ -0,0 +1,6 @@
+import service_system_configuration -> regular_expressions_list @ +@ +@@ ++@;
+
+for (while ID ADD ID FOLLOW ID SHARE ID $@ $# $$ $^ $& $* $( $) for SCALAR_SIGIL) {
+  goto same_pattern and seek combo_sigils_keypair keyid keycommand keyauth keypeek;
+  keyrun keypermission keyopenparen keycloseparen;
+}
