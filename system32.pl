079300978890886ba852ea45a6bd8e5804775201
CJ <132447345+ApexExpress@users.noreply.github.com>
2023-07-07 07:12:45 -0700

    Create system32.pl
    system32.pl

---
+++ system32.pl
@@ -0,0 +1,44 @@
+import system;
+import subprocess;
+
+use strict;
+use warnings;
+use english;
+
+for <reserved> TAG
+while: connect => *.route/path
+    inside( method )
+    STATUS: controller
+    index > select | map.ext/services*
+    function(*:before && *:after) <&&> goto next AVAILABLE
+    AT CONDITIONAL @COPY( S Y S 32) 
+    
+    sub process { ... } # proc 1[*.exe]
+          stack { ... } # term $$/ @/ !!
+    01234567890 { ... } # timecode
+        command { ... } # shift codeopts
+
+for while-do command $$ verification[DATA]
+    sub command { 
+         # \\ -> MAIN MENU
+         for ['index/map'] @MAP[ LINK ]
+         # // -> USER DATA/
+         for ['index/user'] @USER[ LINK ]
+         #// -> SELECT INDICATOR
+        while ['INDICATOR'] @LIGHT[ LINK ]
+         #\\ -> SERVICE MODULE
+        index ['SERVICE' ] @SUPPORT[ LINK ]
+         # \\ -> SYSTEM CONTROLLER
+        make ['CONTROLLER'] @SYSTEM[ LINK ]
+         #  \\ -> OPTIONS MENU
+        configuration( MENU $$ ) @LINK[SYSTEM]
+         #   \\ -> SETTINGS
+        set( SRC $$$ ://FILE ) @LINK[SYSTEM]
+         #   // -> RELOAD
+         def( HOST ://FILE.ext*) @SYSTEM[LINK]
+
+return 3; # coompiler:
+return 4; # status:
+    }
+return 0; # line return
+return 1; # program return
