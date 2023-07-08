abd9866f29e650b8af0e81f71db6c2d8f3d16a01
CJ <132447345+ApexExpress@users.noreply.github.com>
2023-07-08 08:50:41 -0700

    Create Hash_rate_rust.rs
    create hash rate written in run via chat cpt open source

---
+++ Hash_rate_rust.rs
@@ -0,0 +1,19 @@
+extern crate time;
+
+use std::time::{Duration, Instant};
+
+fn main() {
+    let start = Instant::now();
+    let iterations = 1_000_000;
+
+    // Perform the hash calculations
+    for _ in 0..iterations {
+        // Replace this with your actual hash function
+        let _hash_result = hash_function("data_to_hash");
+    }
+
+    let elapsed = start.elapsed();
+    let duration_secs = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9;
+    let hash_rate = (iterations as f64 / duration_secs) as u64;
+
+    println!("Hash rate: {} hashes/second",
